50e336b99dabf3a829f8450bb8ed5b3751463112
“wisdom” <wisdomfollygan@gmail.com>
2025-05-09 09:39:35 +0100

    dernier commit de ce jour

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -14,11 +14,7 @@
       content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
 
     <title>Envoi d'échéances</title>
-
-      <link rel="stylesheet" href="//cdn.datatables.net/2.2.2/css/dataTables.dataTables.min.css">
-
-
-
+      
 
       <meta name="description" content="" />
       <meta name="csrf-token" content="{{ csrf_token() }}">
